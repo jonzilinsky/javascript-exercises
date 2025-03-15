05a0a670e2c2f15fd52443ebddef2964473c08fd
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-14 19:21:10 -1000

    Passed all tests in 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num == 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    } else { 
+        let sstring = string; 
+        for (let i = 1; i < num; i++) {
+            sstring = sstring + string    
+        }
+        return sstring; 
+    }
+}
 
-};
+
+console.log(repeatString('123',3))
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
