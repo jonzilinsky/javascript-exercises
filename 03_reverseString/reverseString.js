8e38236bd11f8aed99d92557d16d5afbbeca4aa8
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-15 10:19:37 -1000

    Passed all tests for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(someString) {
+    let someArray = someString.split("");    
+    let someReverse = someArray.reverse(); 
+    let someJoin = someReverse.join(''); 
+    return someJoin; 
+
+
+
+
 
 };
+
+console.log(reverseString('Hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
