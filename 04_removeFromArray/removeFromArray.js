28ae8934147647b17f02132b3df080d064550e40
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-15 12:28:21 -1000

    Passed all tests on 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(someArray, ...someArgs) {
+    let newArray = []
+    for (const i of someArray) {
+        if (someArgs.includes(i)) {
+        } else {
+            newArray.push(i);
+        }
+    };
+    return (newArray);
+
+
+
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
