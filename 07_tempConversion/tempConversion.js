2cbcea88baf70444e7d671b8d8bcdfac7b004feb
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-15 18:24:41 -1000

    Passed all tests for 07_tempConverstion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahNumber) {
+    result = (fahNumber - 32) * 5 / 9;
+    result = Math.round(result * 10) / 10 
+    return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celNumber) {
+    result = celNumber * 9 / 5 + 32;
+    result = Math.round(result * 10) / 10 
+    return result;
+
 };
+
+/* console.log(convertToCelsius(89));
+   console.log(convertToFahrenheit(32)); */
 
 // Do not edit below this line
 module.exports = {
