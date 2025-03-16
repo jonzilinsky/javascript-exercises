b49e60402a9fcc3407a725a62fbc73446274ea5d
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-15 15:13:38 -1000

    Passed all tests for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    const theArray = [];
+    let theResult = 0;
+    if (first % 1 != 0 || second % 1 != 0) {
+        return 'ERROR';
+    } else if (first > 0 && second > 0) {
+        if (second > first) {
+            for (let i = first; i <= second; i++) {
+                theArray.push(i);
+            }
+            for (const i of theArray) {
+                theResult = theResult + i;
+            }
+            return theResult;
+        } else {
+            for (let i = second; i <= first; i++) {
+                theArray.push(i);
+            }
+            for (const i of theArray) {
+                theResult = theResult + i;
+            }
+            return theResult;
+            }
+    } else {
+        return 'ERROR';
+    }
 };
+
+console.log(sumAll(1,2))
 
 // Do not edit below this line
 module.exports = sumAll;
