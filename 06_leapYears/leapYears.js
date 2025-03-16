f1b937035dee01c3837cd63026766a0bd9764bd4
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-03-15 17:15:14 -1000

    Passed all tests for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (inputYear % 400 == 0) {
+        return true;
+    } else if (inputYear % 100 == 0) {
+        return false;
+    } else if (inputYear % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+/*console.log(leapYears(1600));*/
 
 // Do not edit below this line
 module.exports = leapYears;
